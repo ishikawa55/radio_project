06f188d0f4e55905bb4033ff524b967d834ab756
ishikawa55 <paranaziparanan@gmail.com>
2024-12-30 23:54:23 +0900

    add

--- radio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return render(request, 'radio/index.html')
-
-def play(request):
-    return render(request, 'radio/play.html')
